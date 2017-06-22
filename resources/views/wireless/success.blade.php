a836243dddd1d07a9daebc63f663e621058bc02e
komcommy <joel@vdwouw.me>
2017-06-22 17:24:37 +0200

    Updated style.

--- resources/views/wireless/success.blade.php
+++ resources/views/wireless/success.blade.php
@@ -1,6 +1,16 @@
-<center>
-    <img src="http://scoutroam.org/scoutroam-web.png">
+<head>
+    <title>Scoutroam Connected</title>
 
-    <h1>Success</h1>
-    U bent succesvol verbonden!
-</center>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css">
+</head>
+
+<br><br>
+<div class="container" style="text-align: center">
+    <div class="jumbotron">
+        <img src="http://scoutroam.org/scoutroam-web.png">
+        <h1>Scoutroam Passpoint</h1>
+        <div class="alert alert-success" role="alert"><span class="glyphicon glyphicon-ok"></span> U bent succesvol verbonden!</div>
+
+        <button type="button" class="btn btn-primary btn-lg" href="http://4dk.nl/wireless/terms">Voorwaarden</button>
+    </div>
+</div>
