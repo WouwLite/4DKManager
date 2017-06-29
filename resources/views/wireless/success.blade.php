c1011a307c89a3f0a49a10197355c852fdb51537
komcommy <joel@vdwouw.me>
2017-06-29 11:34:56 +0200

    Updated hotspot views

--- resources/views/wireless/success.blade.php
+++ resources/views/wireless/success.blade.php
@@ -8,7 +8,7 @@
 <div class="container-float" style="text-align: center">
     <div class="jumbotron">
         <img src="http://scoutroam.org/scoutroam-web.png" style="max-width: 100%">
-        <h1>Scouting 4DaagseKamp 2017</h1>
+        <h1>Scoutroam Passpoint</h1>
         <div class="alert alert-success" role="alert"><span class="glyphicon glyphicon-ok"></span> U bent succesvol verbonden!</div>
 
         <a type="button" class="btn btn-primary btn-lg" href="/wireless/rules">Regels</a>
