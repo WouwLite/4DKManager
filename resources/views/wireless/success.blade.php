44bfca32448438678d995368d4936c543f8e3090
komcommy <joel@vdwouw.me>
2017-06-29 11:36:46 +0200

    Updated hotspot views

--- resources/views/wireless/success.blade.php
+++ resources/views/wireless/success.blade.php
@@ -9,7 +9,7 @@
     <div class="jumbotron">
         <img src="http://scoutroam.org/scoutroam-web.png" style="max-width: 100%">
         <h1>Scoutroam Passpoint</h1>
-        <div class="alert alert-success" role="alert"><span class="glyphicon glyphicon-ok"></span> U bent succesvol verbonden!</div>
+        <div class="alert alert-success" role="alert"><h2><span class="glyphicon glyphicon-ok"></span> U bent succesvol verbonden</h2></div>
 
         <a type="button" class="btn btn-primary btn-lg" href="/wireless/rules">Regels</a>
     </div>
