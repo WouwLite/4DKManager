a1dab23910242330835fde11458cf3e00baa11f8
komcommy <joel@vdwouw.me>
2017-07-08 10:58:56 -0600

    Removed typo

--- resources/views/wireless/success.blade.php
+++ resources/views/wireless/success.blade.php
@@ -1,16 +1,42 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
 <head>
-    <title>Scoutroam Connected</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css">
+    {{--<!-- CSRF Token -->--}}
+    {{--<meta name="csrf-token" content="{{ csrf_token() }}">--}}
+
+    <title>Scoutroam Passpoint</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    {{--<!-- Scripts -->--}}
+    {{--<script>--}}
+        {{--window.Laravel = {!! json_encode([--}}
+            {{--'csrfToken' => csrf_token(),--}}
+        {{--]) !!};--}}
+    {{--</script>--}}
 </head>
 
 <br><br>
 <div class="container-float" style="text-align: center">
     <div class="jumbotron">
-        <img src="http://scoutroam.org/scoutroam-web.png" style="max-width: 100%">
+        {{--<img src="http://scoutroam.org/scoutroam-web.png" style="max-width: 100%">--}}
+        <img src="//4dk.dev/img/scoutroam-web.png" style="max-width: 100%">
         <h1>Scoutroam Passpoint</h1>
-        <div class="alert alert-success" role="alert"><h2><span class="glyphicon glyphicon-ok"></span> U bent succesvol verbonden</h2></div>
+        @if((Auth::user() == true))
+            <div class="alert alert-success" role="alert"><h2><span class="glyphicon glyphicon-ok"></span> Hi {{ Auth::user()->name }}, je bent succesvol verbonden!</h2></div>
+        @else
+            <div class="alert alert-success" role="alert"><h2><span class="glyphicon glyphicon-ok"></span> U bent succesvol verbonden!</h2></div>
+        @endif
 
-        <a type="button" class="btn btn-primary btn-lg" href="/wireless/rules" style="min-width: 50%">Regels</a>
+        @if(Auth::user() == true)
+            <a type="button" class="btn btn-primary btn-lg" href="/wireless/rules" style="min-width: 35%">Regels</a> <a type="button" class="btn btn-default btn-lg" href="/login" style="min-width: 15%"><span class="glyphicon glyphicon-home" aria-hidden="true"></span> Dashboard</a>
+        @else
+            <a type="button" class="btn btn-primary btn-lg" href="/wireless/rules" style="min-width: 35%">Regels</a> <a type="button" class="btn btn-default btn-lg" href="/login" style="min-width: 15%"><span class="glyphicon glyphicon-log-in" aria-hidden="true"></span> Log in</a>
+        @endif
     </div>
 </div>
