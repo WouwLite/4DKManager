4ffab5c99e266627bddcd5cd96ff37f3bc4e19de
komcommy <joel@vdwouw.me>
2017-07-10 23:20:48 -0600

    Redesigned WiFi page

--- resources/views/wireless/rules.blade.php
+++ resources/views/wireless/rules.blade.php
@@ -29,7 +29,7 @@
             <h1>Scoutroam WiFi regels</h1>
 
             <center>
-                <div class="list-group center" style="max-width: 50%;">
+                <div class="list-group center" style="max-width: 90%;">
                     <button type="button" class="list-group-item">Scoutroam is niet verantwoordelijk voor enig verlies van informatie of schade aan uw apparatuur als gevolg van het gebruik van het netwerk. De gebruiker blijft verantwoordelijk voor de veiligheid van zijn apparatuur, laptopconfiguraties, verwijderingen van beveiliging of wijzigingen aan bestanden als gevolg van het gebruik van het netwerk.</button>
                     <button type="button" class="list-group-item">Het is niet toegestaan informatie met een illegale aard over ons netwerk te versturen.</button>
                     <button type="button" class="list-group-item">Scoutroam heeft het recht het netwerk op ieder moment te deactiveren, om onderhoud te verrichten en om gelijktijdig gebruik te verdelen.</button>
@@ -37,7 +37,7 @@
                     <button type="button" class="list-group-item">Scoutroam kan informatie verzamelen over uw surfgedrag ten behoeve van analytische doeleinden en beheer van het netwerk. Er worden geen persoonlijke gegevens bewaard.</button>
                 </div>
                 <br>
-                <div class="list-group center" style="max-width: 50%;">
+                <div class="list-group center" style="max-width: 90%;">
                     <button type="button" class="list-group-item">Vragen? Mail naar <b>beheer@scoutroam.org</b> of ga langs bij de <b>infodesk</b> van het evenement</button>
                 </div>
             </center>
--- resources/views/wireless/success.blade.php
+++ resources/views/wireless/success.blade.php
@@ -5,39 +5,102 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    {{--<!-- CSRF Token -->--}}
-    {{--<meta name="csrf-token" content="{{ csrf_token() }}">--}}
-
     <title>Scoutroam Passpoint</title>
 
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Raleway', sans-serif;
+            font-weight: 100;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 84px;
+        }
+
+        .links > a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 12px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet">
-
-    {{--<!-- Scripts -->--}}
-    {{--<script>--}}
-        {{--window.Laravel = {!! json_encode([--}}
-            {{--'csrfToken' => csrf_token(),--}}
-        {{--]) !!};--}}
-    {{--</script>--}}
 </head>
+<body>
+<div class="flex-center position-ref full-height">
+    @if (Route::has('login'))
+        <div class="top-right links">
+            @if (Auth::check())
+                <a href="{{ url('/home') }}">Dashboard</a>
+            @else
+                <a href="{{ url('/login') }}">Aanmelden</a>
+                <a href="{{ url('/register') }}">Registreren</a>
+            @endif
+        </div>
+    @endif
 
-<br><br>
-<div class="container-float" style="text-align: center">
-    <div class="jumbotron">
-        {{--<img src="http://scoutroam.org/scoutroam-web.png" style="max-width: 100%">--}}
-        <img src="//4dk.dev/img/scoutroam-web.png" style="max-width: 100%">
-        <h1>{{ config('app.name', 'Scoutroam Passpoint') }}</h1>
-        @if((Auth::user() == true))
-            <div class="alert alert-success" role="alert"><h2><span class="fa fa-check"></span> Hi {{ Auth::user()->name }}, je bent succesvol verbonden!</h2></div>
-        @else
-            <div class="alert alert-success" role="alert"><h2><span class="fa fa-checkk"></span> U bent succesvol verbonden!</h2></div>
-        @endif
+    <div class="content">
+        <div class="title m-b-md">
+            <img src="//4dk.dev/img/scoutroam-web.png" style="max-width: 100%; margin-top: 25px">
+            <h1>{{ config('app.name', 'Scoutroam Passpoint') }}</h1>
+            @if((Auth::user() == true))
+                <div class="alert alert-success" role="alert"><h2><span class="fa fa-check"></span> Hi {{ Auth::user()->name }}, je bent succesvol verbonden!</h2></div>
+            @else
+                <div class="alert alert-success" role="alert"><h2><span class="fa fa-checkk"></span> U bent succesvol verbonden!</h2></div>
+            @endif
 
-        @if(Auth::user() == true)
-            <a type="button" class="btn btn-primary btn-lg" href="/wireless/rules" style="min-width: 35%">Regels</a> <a type="button" class="btn btn-default btn-lg" href="/login" target="_blank" style="min-width: 15%"><span class="fa fa-dashboard" aria-hidden="true"></span> Dashboard</a>
-        @else
-            <a type="button" class="btn btn-primary btn-lg" href="/wireless/rules" style="min-width: 35%">Regels</a> <a type="button" class="btn btn-default btn-lg" href="/login" target="_blank" style="min-width: 15%"><span class="fa fa-user" aria-hidden="true"></span> Log in</a>
-        @endif
+            <a type="button" class="btn btn-primary btn-lg" href="/wireless/rules" style="min-width: 80%">Regels</a>
+        </div>
+
+        <div class="links">
+            <a href="{{ url('/pages/campinfo') }}">Kampinfo</a>
+            <a href="{{ url('/pages/map') }}">Plattegrond</a>
+            <a href="{{ url('/pages/news') }}">Nieuws</a>
+            <a href="{{ url('/pages/about') }}">Over ons</a>
+            <a href="{{ url('/pages/contact') }}">Contact</a>
+        </div>
     </div>
 </div>
+</body>
+</html>
